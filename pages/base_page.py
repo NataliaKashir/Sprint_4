8272d2a7f9746969c949ddb7b78a1ed446464d30
Natalia Kashirina <ruta0010@yandex.ru>
2023-03-26 16:36:28 +0300

    Commit Spint4 added classes to main_page_locator

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,6 @@
 import allure
-from locators.main_page_locator import *
-from locators.base_page_locator import *
+from locators.main_page_locator import MainPageBottomElements as MPB
+from locators.main_page_locator import MainPageQuestionsElements as MPQ
 from urls import *
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
@@ -18,15 +18,15 @@ class BasePageScooter:
     @allure.step('Ожидаем загрузку главной страницы')
     def wait_for_load_main_page(self):
         # ставлю ожидание 10, потому что Яндекс Самокат открывается оооочень долго в Firefix (у меня)
-        WebDriverWait(self.driver, 10).until(expected_conditions.presence_of_element_located(question_7))
+        WebDriverWait(self.driver, 10).until(expected_conditions.presence_of_element_located(MPQ.question_7))
 
     @allure.step('Скролл до нижней кнопки Заказать')
     def scroll_to_bottom_order_btn(self):
-        bottom_btn = self.driver.find_element(*order_scooter_bottom_btn)
+        bottom_btn = self.driver.find_element(*MPB.order_scooter_bottom_btn)
         self.driver.execute_script("arguments[0].scrollIntoView();", bottom_btn)
 
     @allure.step('Ожидание отображения нижней кнопки Заказать')
     def wait_for_visibility_bottom_order_btn(self):
-        WebDriverWait(self.driver, 3).until(expected_conditions.visibility_of_element_located(order_scooter_bottom_btn))
+        WebDriverWait(self.driver, 3).until(expected_conditions.visibility_of_element_located(MPB.order_scooter_bottom_btn))
 
 
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,6 @@
 import allure
-from locators.main_page_locator import *
+from locators.main_page_locator import MainPageQuestionsElements as MPQ
+from locators.main_page_locator import MainPageHeaderElements as MPH
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -11,120 +12,120 @@ class MainPageScooter:
 
     @allure.step('Ожидание отображения последнего элемента вопроса на главной странице')
     def wait_for_scroll_main_page(self):
-        WebDriverWait(self.driver, 3).until(expected_conditions.visibility_of_element_located(question_7))
+        WebDriverWait(self.driver, 3).until(expected_conditions.visibility_of_element_located(MPQ.question_7))
 
     @allure.step('Скрол до последнего элемента вопроса на главной странице')
     def scroll_to_questions(self):
-        self.driver.execute_script("arguments[0].scrollIntoView();", self.driver.find_element(*question_7))
+        self.driver.execute_script("arguments[0].scrollIntoView();", self.driver.find_element(*MPQ.question_7))
 
     @allure.step('Клик по вопросу 0')
     def click_question_0_div(self):
-        self.driver.find_element(*question_0).click()
+        self.driver.find_element(*MPQ.question_0).click()
 
     @allure.step('Клик по вопросу 1')
     def click_question_1_div(self):
-        self.driver.find_element(*question_1).click()
+        self.driver.find_element(*MPQ.question_1).click()
 
     @allure.step('Клик по вопросу 2')
     def click_question_2_div(self):
-        self.driver.find_element(*question_2).click()
+        self.driver.find_element(*MPQ.question_2).click()
 
     @allure.step('Клик по вопросу 3')
     def click_question_3_div(self):
-        self.driver.find_element(*question_3).click()
+        self.driver.find_element(*MPQ.question_3).click()
 
     @allure.step('Клик по вопросу 4')
     def click_question_4_div(self):
-        self.driver.find_element(*question_4).click()
+        self.driver.find_element(*MPQ.question_4).click()
 
     @allure.step('Клик по вопросу 5')
     def click_question_5_div(self):
-        self.driver.find_element(*question_5).click()
+        self.driver.find_element(*MPQ.question_5).click()
 
     @allure.step('Клик по вопросу 6')
     def click_question_6_div(self):
-        self.driver.find_element(*question_6).click()
+        self.driver.find_element(*MPQ.question_6).click()
 
     @allure.step('Клик по вопросу 7')
     def click_question_7_div(self):
-        self.driver.find_element(*question_7).click()
+        self.driver.find_element(*MPQ.question_7).click()
 
     @allure.step('Получаем текст к по вопросу 0')
     def get_question_0_text(self):
-        return self.driver.find_element(*answer_0_p).text
+        return self.driver.find_element(*MPQ.answer_0_p).text
 
     @allure.step('Получаем текст к по вопросу 1')
     def get_question_1_text(self):
-        return self.driver.find_element(*answer_1_p).text
+        return self.driver.find_element(*MPQ.answer_1_p).text
 
     @allure.step('Получаем текст к по вопросу 2')
     def get_question_2_text(self):
-        return self.driver.find_element(*answer_2_p).text
+        return self.driver.find_element(*MPQ.answer_2_p).text
 
     @allure.step('Получаем текст к по вопросу 3')
     def get_question_3_text(self):
-        return self.driver.find_element(*answer_3_p).text
+        return self.driver.find_element(*MPQ.answer_3_p).text
 
     @allure.step('Получаем текст к по вопросу 4')
     def get_question_4_text(self):
-        return self.driver.find_element(*answer_4_p).text
+        return self.driver.find_element(*MPQ.answer_4_p).text
 
     @allure.step('Получаем текст к по вопросу 5')
     def get_question_5_text(self):
-        return self.driver.find_element(*answer_5_p).text
+        return self.driver.find_element(*MPQ.answer_5_p).text
 
     @allure.step('Получаем текст к по вопросу 6')
     def get_question_6_text(self):
-        return self.driver.find_element(*answer_6_p).text
+        return self.driver.find_element(*MPQ.answer_6_p).text
 
     @allure.step('Получаем текст к по вопросу 7')
     def get_question_7_text(self):
-        return self.driver.find_element(*answer_7_p).text
+        return self.driver.find_element(*MPQ.answer_7_p).text
 
     @allure.step('Получаем элемент ответа 0')
     def get_answer_0_div(self):
-        return self.driver.find_element(*answer_0_div)
+        return self.driver.find_element(*MPQ.answer_0_div)
 
     @allure.step('Получаем элемент ответа 1')
     def get_answer_1_div(self):
-        return self.driver.find_element(*answer_1_div)
+        return self.driver.find_element(*MPQ.answer_1_div)
 
     @allure.step('Получаем элемент ответа 2')
     def get_answer_2_div(self):
-        return self.driver.find_element(*answer_2_div)
+        return self.driver.find_element(*MPQ.answer_2_div)
 
     @allure.step('Получаем элемент ответа 3')
     def get_answer_3_div(self):
-        return self.driver.find_element(*answer_3_div)
+        return self.driver.find_element(*MPQ.answer_3_div)
 
     @allure.step('Получаем элемент ответа 4')
     def get_answer_4_div(self):
-        return self.driver.find_element(*answer_4_div)
+        return self.driver.find_element(*MPQ.answer_4_div)
 
     @allure.step('Получаем элемент ответа 5')
     def get_answer_5_div(self):
-        return self.driver.find_element(*answer_5_div)
+        return self.driver.find_element(*MPQ.answer_5_div)
 
     @allure.step('Получаем элемент ответа 6')
     def get_answer_6_div(self):
-        return self.driver.find_element(*answer_6_div)
+        return self.driver.find_element(*MPQ.answer_6_div)
 
     @allure.step('Получаем элемент ответа 7')
     def get_answer_7_div(self):
-        return self.driver.find_element(*answer_7_div)
+        return self.driver.find_element(*MPQ.answer_7_div)
 
     @allure.step('Клик по надписи Самокат в логотипе')
     def click_logo_scooter(self):
-        self.driver.find_element(*logo_scooter).click()
+        self.driver.find_element(*MPH.logo_scooter).click()
 
     @allure.step('Клик по надписи Яндекс в логотипе')
     def click_logo_yandex(self):
-        self.driver.find_element(*logo_yandex).click()
+        self.driver.find_element(*MPH.logo_yandex).click()
 
     @allure.step('Клик по верхней кнопке Заказать')
     def click_top_order_btn(self):
-        self.driver.find_element(*order_scooter_top_btn).click()
+        self.driver.find_element(*MPH.order_scooter_top_btn).click()
 
     @allure.step('Клик по нижней кнопке Заказать')
     def click_bottom_order_btn(self):
-        self.driver.find_element(*order_scooter_bottom_btn).click()
+        self.driver.find_element(*MPH.order_scooter_bottom_btn).click()
