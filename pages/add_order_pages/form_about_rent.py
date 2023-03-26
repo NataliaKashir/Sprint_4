0cc304c23ca456943856b84722ee06cfbaaeeb2f
Natalia Kashirina <ruta0010@yandex.ru>
2023-03-26 15:24:18 +0300

    Commit Spint4 methods divided by empty line

--- pages/add_order_pages/form_about_rent.py
+++ pages/add_order_pages/form_about_rent.py
@@ -1,5 +1,4 @@
 import allure
-import time
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from test_data import *
@@ -13,6 +12,7 @@ class FormPageAboutRentScooter:
     @allure.step('Ожидаем загрузку страницы Про аренду')
     def wait_for_load_add_order_about_rent_page(self):
         WebDriverWait(self.driver, 10).until(expected_conditions.presence_of_element_located(order_btn))
+
     @allure.step('Ожидаем появление окна страницы Хотите оформить заказ?')
     def wait_confirmation_order_window(self):
         WebDriverWait(self.driver, 10).until(expected_conditions.presence_of_element_located(order_btn_confirm))
@@ -31,24 +31,31 @@ class FormPageAboutRentScooter:
     @allure.step('Нажимаем кнопку Далее на форме Про аренду')
     def click_order_scooter_about_rent_form_next_btn(self):
         self.driver.find_element(*order_btn).click()
+
     @allure.step('Получаем установленное значение поля Когда привезти самокат')
     def get_about_rent_form_date(self):
         return self.driver.find_element(*order_date).get_property("value")
+
     @allure.step('Получаем установленное значение поля Срок аренды')
     def get_about_rent_form_days(self):
         return self.driver.find_element(*order_days_selected).text
+
     @allure.step('Получаем установленное значение поля Цвет')
     def get_about_rent_form_color(self, color_input):
         return self.driver.find_element(*color_input).get_property("checked")
+
     @allure.step('Получаем установленное значение поля Когмментарий')
     def get_about_rent_form_comment(self):
         return self.driver.find_element(*order_comment).get_property("value")
+
     @allure.step('Нажимаем кнопку Заказать в форме Про аренду')
     def click_about_rent_form_order_btn(self):
         return self.driver.find_element(*order_btn).click()
+
     @allure.step('Нажимаем кнопку подтверждения заказа')
     def click_order_btn_confirm(self):
         return self.driver.find_element(*order_btn_confirm).click()
+
     @allure.step('Получаем текст заголовка всплывающего окна заказа')
     def get_ordered_popup_window_header(self):
         return self.driver.find_element(*ordered_header).text
--- pages/main_page.py
+++ pages/main_page.py
@@ -20,24 +20,31 @@ class MainPageScooter:
     @allure.step('Клик по вопросу 0')
     def click_question_0_div(self):
         self.driver.find_element(*question_0).click()
+
     @allure.step('Клик по вопросу 1')
     def click_question_1_div(self):
         self.driver.find_element(*question_1).click()
+
     @allure.step('Клик по вопросу 2')
     def click_question_2_div(self):
         self.driver.find_element(*question_2).click()
+
     @allure.step('Клик по вопросу 3')
     def click_question_3_div(self):
         self.driver.find_element(*question_3).click()
+
     @allure.step('Клик по вопросу 4')
     def click_question_4_div(self):
         self.driver.find_element(*question_4).click()
+
     @allure.step('Клик по вопросу 5')
     def click_question_5_div(self):
         self.driver.find_element(*question_5).click()
+
     @allure.step('Клик по вопросу 6')
     def click_question_6_div(self):
         self.driver.find_element(*question_6).click()
+
     @allure.step('Клик по вопросу 7')
     def click_question_7_div(self):
         self.driver.find_element(*question_7).click()
@@ -45,24 +52,31 @@ class MainPageScooter:
     @allure.step('Получаем текст к по вопросу 0')
     def get_question_0_text(self):
         return self.driver.find_element(*answer_0_p).text
+
     @allure.step('Получаем текст к по вопросу 1')
     def get_question_1_text(self):
         return self.driver.find_element(*answer_1_p).text
+
     @allure.step('Получаем текст к по вопросу 2')
     def get_question_2_text(self):
         return self.driver.find_element(*answer_2_p).text
+
     @allure.step('Получаем текст к по вопросу 3')
     def get_question_3_text(self):
         return self.driver.find_element(*answer_3_p).text
+
     @allure.step('Получаем текст к по вопросу 4')
     def get_question_4_text(self):
         return self.driver.find_element(*answer_4_p).text
+
     @allure.step('Получаем текст к по вопросу 5')
     def get_question_5_text(self):
         return self.driver.find_element(*answer_5_p).text
+
     @allure.step('Получаем текст к по вопросу 6')
     def get_question_6_text(self):
         return self.driver.find_element(*answer_6_p).text
+
     @allure.step('Получаем текст к по вопросу 7')
     def get_question_7_text(self):
         return self.driver.find_element(*answer_7_p).text
@@ -70,24 +84,31 @@ class MainPageScooter:
     @allure.step('Получаем элемент ответа 0')
     def get_answer_0_div(self):
         return self.driver.find_element(*answer_0_div)
+
     @allure.step('Получаем элемент ответа 1')
     def get_answer_1_div(self):
         return self.driver.find_element(*answer_1_div)
+
     @allure.step('Получаем элемент ответа 2')
     def get_answer_2_div(self):
         return self.driver.find_element(*answer_2_div)
+
     @allure.step('Получаем элемент ответа 3')
     def get_answer_3_div(self):
         return self.driver.find_element(*answer_3_div)
+
     @allure.step('Получаем элемент ответа 4')
     def get_answer_4_div(self):
         return self.driver.find_element(*answer_4_div)
+
     @allure.step('Получаем элемент ответа 5')
     def get_answer_5_div(self):
         return self.driver.find_element(*answer_5_div)
+
     @allure.step('Получаем элемент ответа 6')
     def get_answer_6_div(self):
         return self.driver.find_element(*answer_6_div)
+
     @allure.step('Получаем элемент ответа 7')
     def get_answer_7_div(self):
         return self.driver.find_element(*answer_7_div)
@@ -95,6 +116,7 @@ class MainPageScooter:
     @allure.step('Клик по надписи Самокат в логотипе')
     def click_logo_scooter(self):
         self.driver.find_element(*logo_scooter).click()
+
     @allure.step('Клик по надписи Яндекс в логотипе')
     def click_logo_yandex(self):
         self.driver.find_element(*logo_yandex).click()
